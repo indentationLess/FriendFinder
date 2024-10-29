b66a0b92032b4c01bcca9d4ac4fa5ccc396986f3
euphrosyne <urfavdigitalperson@gmail.com>
2024-10-29 11:37:25 +0300

    Refactor server.js and docker-compose.yml - it all now connects to eachother using docker

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;

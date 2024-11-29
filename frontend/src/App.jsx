5d4fa28f38a983023ba5747c2bdfe04a85e6234e
euphrosyne <urfavdigitalperson@gmail.com>
2024-11-29 13:38:36 +0200

    done with the sign up form, migrated back from materialUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignupForm from './components/signupform';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="h-screen flex flex-row items-center text-blue-200 p-4">
+    <Router>
       <NavBar className="fixed top-0 w-full" />
       <div className="mt-16">
+        <Routes>
+          <Route path="/login" element={<SignupForm />} />
+          <Route
+            path="/"
+            element={
+              <div className="h-screen flex flex-row items-center text-blue-200 p-4">
+                {/* Main content */}
+              </div>
+            }
+          />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import Button from '@mui/material/Button';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isSignupPage = location.pathname === "/login";
+
   return (
-    <nav className="fixed top-0 left-0 w-full flex items-center justify-between bg-blue-500 p-4">
+    <nav className="fixed top-0 left-0 w-full flex items-center justify-between bg-azure p-4">
       <div className="flex-1">
-        <Button variant="contained" color="primary" className="bg-blue-700">
-          Friend Finder
-        </Button>
+        <img
+          src="/logo.png"
+          alt="logo icon"
+          className="h-20 w-auto sm:h-12 sm:w-auto mr-2"
+        />
       </div>
-      <div className="flex-1 flex justify-end space-x-4">
-        <Button variant="contained" color="primary" className="bg-blue-700">
-          Edit Profile
-        </Button>
-        <Button variant="contained" color="primary" className="bg-blue-700">
-          Sign In
-        </Button>
-      </div>
+      {!isSignupPage && (
+        <div className="flex-1 flex justify-end space-x-4">
+          <button className="bg-blue-500 text-white py-2 px-6 rounded-full hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:outline-none">
+            Edit Profile
+          </button>
+          <button
+            onClick={() => navigate("/login")}
+            className="bg-blue-500 text-white py-2 px-6 rounded-full hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+          >
+            Sign In
+          </button>
+        </div>
+      )}
     </nav>
   );
 }
+
 export default NavBar;

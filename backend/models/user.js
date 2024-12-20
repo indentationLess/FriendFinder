19035cbe5577637911aca2f370a213a4caa58593
euphrosyne <urfavdigitalperson@gmail.com>
2024-12-20 02:45:12 +0200

    add basic user, chat, and message mongoose models

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,33 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  bio: String,
-  interests: [String],
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, required: true, enum: ['male', 'female'] },
+  
+  major: { 
+    type: String, 
+    required: true,
+  },
+  graduationYear: { type: Number, required: true },
+  
+  bio: { type: String, maxlength: 500 },
+  profilePicture: { 
+    url: String,
+    contentType: String
+  },
+  
+  music: {
+    favoriteArtists: [String],
+    favoriteGenres: [String]
+  },
+  
+  isActive: { type: Boolean, default: true }
+}, {
+  timestamps: true
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("user", userSchema);

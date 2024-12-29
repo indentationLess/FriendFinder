b40d0aa9142e96c023cc379896e50dc032677b08
euphrosyne <urfavdigitalperson@gmail.com>
2024-12-29 09:24:27 +0200

    trying to fix why docker screams

--- backend/routes/chat.route.js
+++ backend/routes/chat.route.js
@@ -1,11 +1,13 @@
 import express from "express";
-import protect from "../middleware/authorizeuser";
-import {accessChat, fetchChat,createGroup, renameGroups, addToGroup, removeFromGroup} from "../controllers/chat.controller";
+import protect from "../middleware/authorizeuser.js";
+import chatController from "../controllers/chat.controller.js";
+
 const router = express.Router();
-router.route('/').post(protect, accessChat);
-router.route('/').get(protect, fetchChat);
-router.route('/group').post(protect, createGroup);
-router.route('/rename').put(protect, renameGroups);
-router.route('/groupadd').put(protect, addToGroup);
-router.route('/groupremove').put(protect, removeFromGroup);
+router.route('/').post(protect, chatController.accessChat);
+router.route('/').get(protect, chatController.fetchChat);
+router.route('/group').post(protect, chatController.createGroup);
+router.route('/rename').put(protect, chatController.renameGroups);
+router.route('/groupadd').put(protect, chatController.addToGroup);
+router.route('/groupremove').put(protect, chatController.removeFromGroup);
+
 export default router;
--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,6 +1,9 @@
 import express from "express";
-import protect from "../middleware/authorizeuser";
+import protect from "../middleware/authorizeuser.js";
+import { sendMessage, getMessages } from "../controllers/message.controller.js";
+
 const Router = express.Router();
 Router.route('/').post(protect, sendMessage);
-Router.route('/:chatId').get(protect, fetchMessages);
+Router.route('/:chatId').get(protect, getMessages);
+
 export default Router;
